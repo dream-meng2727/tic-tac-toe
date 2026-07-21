7304cb23ce97795c83a6a4c225f0860ea8e533d6
dream-meng2727 <3150250390@qq.com>
2026-07-21 22:00:50 +0800

    添加项目文件。

---
+++ game1/game.c
@@ -0,0 +1,7 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include"game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+
+}
---
+++ game1/game.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#define ROW 3
+#define COL 3
+
+void InitBoard(char board[ROW][COL], int row, int col);
