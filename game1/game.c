934510bafd849981581922ce86e32b3bc9badc3f
dream-meng2727 <3150250390@qq.com>
2026-07-29 18:04:34 +0800

    Complete console tic-tac-toe game logic

--- game1/game.c
+++ game1/game.c
@@ -3,5 +3,151 @@
 
 void InitBoard(char board[ROW][COL], int row, int col)
 {
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < row;i++)
+	{
+		for (j = 0;j < col;j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
 
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < row;i++)
+	{
+		//打印数据
+		//printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+		for (j = 0;j < col;j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+				printf("|");
+		}
+		printf("\n");
+		//打印分隔符
+		//printf("---|---|---\n");
+		if (i < row - 1)
+		{
+			for (j = 0;j < col;j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家请下棋：");
+
+	while (1)
+	{
+		printf("请输入坐标：");
+		scanf("%d %d", &x, &y);
+		//坐标合法性判断
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			//下棋
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("坐标被占用，请重新输入：");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入\n");
+		}
+	}
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	printf("电脑下棋：\n");
+
+	int x = 0;
+	int y = 0;
+
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < row;i++)
+	{
+		for (j = 0;j < col;j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	//判断行
+	int i = 0;
+	for (i = 0;i < row ;i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+		{
+			return board[i][1];
+		}
+	}
+	//判断列
+	 int j = 0;
+	for (j = 0;j < row;j++)
+	{
+		if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[1][j] != ' ')
+		{
+			return board[1][j];
+		}
+	}
+	//判断对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+
+	//判断平局
+	if (IsFull(board, row, col))
+	{
+		return 'Q';
+	}
+
+	//游戏继续
+	return 'C';
 }
--- game1/test.c
+++ game1/test.c
@@ -1,6 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
-#include<stdio.h>
 #include"game.h"
 
 void menu()
@@ -13,17 +12,61 @@ void menu()
 
 void game()
 {
+	char ret = 0;
 	char board[ROW][COL] = { 0 };
 	//初始化棋盘
 	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board,ROW,COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+
+		//打印棋盘
+		DisplayBoard(board, ROW, COL);
+		//电脑下棋
+		ComputerMove(board,ROW,COL);
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+
+		//打印棋盘
+		DisplayBoard(board, ROW, COL);
+
+	}
+	DisplayBoard(board, ROW, COL);
+	if (ret == '*')
+	{
+		printf("恭喜你，获胜了！\n");
+	}
+	else if (ret == '#')
+	{
+		printf("很遗憾，你输了！\n");
+	}
+	else
+	{
+		printf("平局！\n");
+	}
 }
 
 int main()
 {
+	srand((unsigned)time(NULL));//设置随机数生成的起点
 	int input=0;
 	do 
 	{
-		menu();
+		menu();//打印菜单
 		printf("请选择:");
 		scanf("%d", &input);
 		switch (input)
